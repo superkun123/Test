15d1a54eb899ba0727d0b6a4fa9744d97885723a
Egor <kolyadintsev1997@mail.ru>
2019-05-27 15:58:59 +0500

    First commint

---
+++ lesson1/script.js
@@ -0,0 +1,20 @@
+
+// let persone = {
+//     name:  'john',
+//     age:  25
+
+// };
+
+
+// console.log(persone);
+
+let testString = (4);
+let testString2 = (2);
+
+let summ = (testString+testString2);
+
+console.log (summ);
+
+
+
+
